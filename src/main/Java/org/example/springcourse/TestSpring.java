b9184550678048484c2c78c03b6dd6dfc66c2cf3
Selistral <NeksusVita404@mail.ru>
2020-01-27 07:27:46 +0300

    Продолжение ДЗ

--- src/main/Java/org/example/springcourse/TestSpring.java
+++ src/main/Java/org/example/springcourse/TestSpring.java
@@ -1,10 +1,13 @@
 package org.example.springcourse;
 import org.springframework.stereotype.Component;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+/// Роль в этой связки что в JavaSpring будут хрониться как пороли так и логи для общения между платформами
+@SpringBootApplication
 public class TestSpring {
     public static void main(String[] args){
-
+/* Это к Курсочу дела не имеет (Это отдельная учёба которая идёт в паралели, когда на неё у меня время находиться)
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserAdmin user = context.getBean("UserAdmin", UserAdmin.class);
         Translate trans = context.getBean("Translate", Translate.class);
@@ -14,5 +17,7 @@ public class TestSpring {
         normal.UserName();
         context.close();
 
-    }
+*/
+ SpringApplication.run(TestSpring.class, args); // Само парсирование класса
+};
 };
