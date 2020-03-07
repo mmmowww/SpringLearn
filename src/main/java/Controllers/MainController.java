eff17bdc986ed514b253c21ca4e8ee5b89cd8b43
Selistral <NeksusVita404@mail.ru>
2020-03-07 19:29:56 +0300

    JavaSpring = Laravel.Routing

---
+++ src/main/java/Controllers/MainController.java
@@ -0,0 +1,16 @@
+package Controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+    /// Похоже с роутингом как
+    /// D laravel
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "General");
+        return "home";  // Имя шаблона
+    }
+}
--- src/main/java/com/MMMOWWW/Neksus/NeksusApplication.java
+++ src/main/java/com/MMMOWWW/Neksus/NeksusApplication.java
@@ -9,5 +9,5 @@ public class NeksusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NeksusApplication.class, args);
 	}
-
+	System.out.println("HELLO");
 }
