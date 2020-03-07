a3d9cc6ffdd333c6455927a6a6057355bfd14f5a
Selistral <NeksusVita404@mail.ru>
2020-03-07 18:05:18 +0300

    GDudar начало уроков MVC

---
+++ src/main/java/com/MMMOWWW/Neksus/NeksusApplication.java
@@ -0,0 +1,13 @@
+package com.MMMOWWW.Neksus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeksusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeksusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MMMOWWW/Neksus/NeksusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MMMOWWW.Neksus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeksusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
